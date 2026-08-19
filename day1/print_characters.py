c89d7fde4c02d15664d09770f38b2f15e6a9bc9e
Myeongju Lee <myeongju0203@naver.com>
2026-08-19 12:16:19 +0900

    day1: 문자열 돌리기 풀이

---
+++ day1/print_characters.py
@@ -0,0 +1,16 @@
+"""
+문제: 문자열 돌리기
+문자열 str이 주어집니다.
+문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+막혔던 부분
+1. 문자열 하나씩 꺼내오는 방법을 까먹음
+"""
+
+
+
+str = input()
+for a in str:
+    print(a)
